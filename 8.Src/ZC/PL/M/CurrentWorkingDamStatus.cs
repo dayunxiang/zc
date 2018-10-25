1e57042b313e4e6856c4976aac1c6b3000b9973c
35982772 <35982772@qq.com>
2018-10-25 15:58:34 +0800

    add current done cycle count status class

--- 8.Src/ZC/PL/M/CurrentWorkingDamStatus.cs
+++ 8.Src/ZC/PL/M/CurrentWorkingDamStatus.cs
@@ -92,4 +92,87 @@ namespace PL
             //
         }
     }
+
+    public class CurrentDoneCycleCountStatus : PlcAddress
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="address"></param>
+        public CurrentDoneCycleCountStatus(string address)
+            : base(address)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Read()
+        {
+            if (Config.IsMock)
+            {
+                return ReadMock();
+            }
+            else
+            {
+                return ReadFact();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private int ReadMock()
+        {
+            //todo:
+            //
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private int ReadFact()
+        {
+            var obj = ReadFromOpc();
+            return Convert.ToInt32(obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="damValue"></param>
+        public void Write(int doneCycleCount)
+        {
+            if (Config.IsMock)
+            {
+                WriteMock(doneCycleCount);
+            }
+            else
+            {
+                WriteFact(doneCycleCount);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void WriteFact(int doneCycleCount)
+        {
+            WriteToOpc(doneCycleCount);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void WriteMock(int damValue)
+        {
+            //todo:
+            //
+        }
+    }
 }
