4e7c1203f83311268715c24f542f4affbb79112b
hkiaipc <hkiaipc@outlook.com>
2020-11-05 16:00:03 +0800

    add cart define

---
+++ 8.Src/ZC/PL/Hardware/CartDefine.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PL.Hardware {
+
+    public class CartDefine {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
