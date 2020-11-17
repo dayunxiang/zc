923e6732951002420f512030c67b844ecf824e5d
hkiaipc <hkiaipc@outlook.com>
2020-11-17 09:09:26 +0800

    remove using

--- 8.Src/ZC/PL/C/AppController.cs
+++ 8.Src/ZC/PL/C/AppController.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Diagnostics;
 using System.Windows.Forms;
-using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using PLC;
 using NLog;
 using PL.Hardware;
