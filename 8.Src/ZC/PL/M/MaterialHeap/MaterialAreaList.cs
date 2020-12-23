c8b78bf28c6beb6936219f97eebcd1a0b9a4d0a1
hkiaipc <hkiaipc@outlook.com>
2020-12-23 14:20:43 +0800

    replace first by firstOrDefault

--- 8.Src/ZC/PL/M/MaterialHeap/MaterialAreaList.cs
+++ 8.Src/ZC/PL/M/MaterialHeap/MaterialAreaList.cs
@@ -7,16 +7,24 @@ using PL.Hardware;
 
 namespace PL {
 
-    public class MaterialAreaList : List<MaterialArea>{
+    public class MaterialAreaList : List<MaterialArea> {
         public MaterialArea GetByName(string materialAreaName) {
-            materialAreaName = materialAreaName.Trim ();
+            if (string.IsNullOrEmpty(materialAreaName)) {
+                throw new ArgumentException("materialAreaName");
+            }
 
-            var ma =  this.First(x => {
+            materialAreaName = materialAreaName.Trim();
+
+            var ma = this.FirstOrDefault(x => {
                 var maName = x.ReadStockGroupIdString();
                 maName = maName.Trim();
                 return StringComparer.OrdinalIgnoreCase.Compare(maName, materialAreaName) == 0;
             });
 
+            if (ma == null) {
+                var s = string.Format("can not find material area by name '{0}'", materialAreaName);
+                throw new ArgumentException (s);
+            }
             return ma;
         }
     }
